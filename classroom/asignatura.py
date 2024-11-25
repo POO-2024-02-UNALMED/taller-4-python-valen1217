4d35f2444f0282c06f384faec0c7e7de93068b9e
valen1217 <102827620+valen1217@users.noreply.github.com>
2024-11-25 22:01:58 +0000

    prueba 1

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,52 @@
-class Asignatura:
+package classroom;
 
-    def __init__(self, nombre, salon):
-        self._nombre = nombre
-        self._salon = salon
+public class Asignatura {
 
-    # def __str__(self):
-    #     pass
+    public String nombre;
+    public int codigoInterno;
+    public int codigoExterno;
+    public Tipo tipo;
+
+    public Asignatura() {
+        this("Sin nombre", Tipo.FUNDAMENTACION);
+    }
+
+    public Asignatura(int codigoExterno, Tipo tipo) {
+        this("Sin nombre", 0, codigoExterno, tipo);
+    }
+
+    public Asignatura(String nombre, Tipo tipo) {
+        this(nombre, 0, 0, tipo);
+    }
+
+    public Asignatura(String nombre, int codigoInterno, int codigoExterno, Tipo tipo) {
+        this.nombre = nombre;
+        this.codigoInterno = codigoInterno;
+        this.codigoExterno = codigoExterno;
+        this.tipo = tipo;
+    }
+
+    public void cambiarDatos(int codigoInterno, int codigoExterno, String nombre, Tipo tipo) {
+        this.codigoInterno = codigoInterno;
+        this.codigoExterno = codigoExterno;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public void cambiarDatos(int codigoExterno) {
+        this.codigoExterno = codigoExterno;
+    }
+
+    public void cambiarDatos(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCodigoInterno(int codigoInterno) {
+        this.codigoInterno = codigoInterno;
+    }
+
+    public void setCodigoInterno(double codigoInterno) {
+        this.codigoInterno = (int) codigoInterno;
+    }
+}
+
