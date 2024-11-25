9dc41291377d59a495dd70723013a765a8fdb99e
valen1217 <102827620+valen1217@users.noreply.github.com>
2024-11-25 22:45:38 +0000

    prueba

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -2,7 +2,7 @@ from classroom.asignatura import Asignatura
 
 class Grupo:
     grado = "Grado 12"  
-    
+
     def _init_(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas if asignaturas is not None else []
