3b47b69343a8b993e98a42b0e19cf5ba5054cf95
valen1217 <102827620+valen1217@users.noreply.github.com>
2024-11-26 02:09:41 +0000

    prueba

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,24 +1,24 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = "Grado 12"  # Valor por defecto
+    grado = "Grado 6"  # Valor inicial
 
-    def _init_(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
-        self._asignaturas = asignaturas if asignaturas is not None else []
-        self.listadoAlumnos = estudiantes if estudiantes is not None else []
+        self._asignaturas = asignaturas if asignaturas else []
+        self.listadoAlumnos = estudiantes if estudiantes else []
 
-    def listadoAsignaturas(self, **kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
+    def agregarAlumno(self, alumno):
+        """Agrega un alumno al listado de alumnos del grupo."""
+        self.listadoAlumnos.append(alumno)
 
-    def agregarAlumno(self, alumno, lista=None):
-        if lista is None:
-            lista = []
-        lista.append(alumno)
-        self.listadoAlumnos += lista
+    def listadoAsignaturas(self, *nombres_asignaturas):
+        """Añade asignaturas con los nombres proporcionados."""
+        for nombre in nombres_asignaturas:
+            self._asignaturas.append(Asignatura(nombre))  # Añadimos asignaturas con los nombres
 
-    def _str_(self):
+    def __str__(self):
+        alumnos_str = ", ".join(self.listadoAlumnos)
         return f"Grupo de estudiantes: {self._grupo}"
 
     @classmethod
